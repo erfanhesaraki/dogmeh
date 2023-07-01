7f6b8be20a207a818f09c01ced665214338434d1
Erfan hesaraki <120978791+erfanhesaraki@users.noreply.github.com>
2023-07-01 13:34:23 -0700

    slider

---
+++ asset/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.main__Slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        fade:true,
+      });   
+  });
