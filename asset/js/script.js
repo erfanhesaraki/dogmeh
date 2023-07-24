8fe03f53eb1d06cb4d99901cd5fd71b6520fee6f
Erfan hesaraki <120978791+erfanhesaraki@users.noreply.github.com>
2023-07-24 18:32:09 +0330

    slick

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,5 +6,44 @@ $(document).ready(function(){
         slidesToShow: 1,
         adaptiveHeight: true,
         fade:true,
-      });   
+      });  
+      
+    
+
+      $('.home__Product_List').slick({
+        dots: false,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true,
+              fade:true,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+
+        ]
+      });
+                      
+      
   });
