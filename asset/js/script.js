67f6d02b2f22a72e8f7a901b23938937bbd0ea49
Erfan hesaraki <120978791+erfanhesaraki@users.noreply.github.com>
2023-07-29 18:04:32 +0330

    takmil slider

--- asset/js/script.js
+++ asset/js/script.js
@@ -10,47 +10,52 @@ $(document).ready(function () {
   );
 
   $(".main__Slider").slick({
-    dots: false,
+    dots: true,
     infinite: true,
-    speed: 300,
+    speed: 3000,
+    autoplaySpeed:3000 ,
     slidesToShow: 1,
     adaptiveHeight: true,
     fade: true,
+    appendArrows:".append__Slider_Arrow",
+    appendDots:".append__Slider_Arrow",
+    arrow:true,
   });
 
   $(".home__Product_List").slick({
     dots: false,
     infinite: true,
-    speed: 200,
+    speed: 3000,
+    autoplaySpeed:500 ,
     slidesToShow: 4,
     slidesToScroll: 4,
+    arrow:true,
     responsive: [
       {
-        breakpoint: 992,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: false,
-        },
+          dots: true
+        }
       },
       {
         breakpoint: 768,
         settings: {
-          infinite: true,
           slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+          slidesToScroll: 2
+        }
       },
       {
         breakpoint: 576,
         settings: {
-          infinite: true,
           slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+          slidesToScroll: 1
+        }
+      }
+    ]
+ 
   });
 
   $(".home__Plugins_List").slick({
